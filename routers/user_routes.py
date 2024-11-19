67fee8e43f53d558eb5cea0f77593de5fb849214
pedro <pedrovictor48@live.com>
2024-11-18 23:12:36 -0300

    adicionar mais campos ao registro

--- routers/user_routes.py
+++ routers/user_routes.py
@@ -2,12 +2,15 @@ from fastapi import APIRouter, HTTPException, Header
 from pydantic import BaseModel, Field
 from auth_utils import get_password_hash, verify_password, create_access_token, decode_token
 from mongo_conn import db 
+from datetime import datetime, timedelta
 
 router = APIRouter()
 
 class CreateUser(BaseModel):
     email: str = Field(..., example="user@example.com")
     password: str = Field(..., example="strongpassword")
+    username: str = Field(..., example="user")
+    birthdate: str = Field(..., example="2000-01-01")
 
 @router.post(
     "/register",
@@ -23,8 +26,22 @@ async def create_user(user: CreateUser):
     if existing_user:
         raise HTTPException(status_code=400, detail="User already exists")
     
+    existing_user = db.user.find_one({"username": user.username})
+    if existing_user:
+        raise HTTPException(status_code=400, detail="Username already exists")
+    
+    #check if the user is under 5 years old
+    birthdate = datetime.strptime(user.birthdate, "%Y-%m-%d")
+    if datetime.now() - birthdate < timedelta(days=5*365):
+        raise HTTPException(status_code=400, detail="User is under 5 years old")
+
     encripted_password = get_password_hash(user.password)
-    db.user.insert_one({"email": user.email, "password": encripted_password})
+    db.user.insert_one({
+        "email": user.email,
+        "password": encripted_password,
+        "username": user.username,
+        "birthdate": user.birthdate
+    })
     return {"message": "User created successfully"}
 
 class LoginUser(BaseModel):
